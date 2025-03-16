4d0d071f5b777ad2cfc4e8cb74284c6d39f38192
Yoel Canul <yoel.canul.yacg@gmail.com>
2025-03-15 20:34:27 -0500

    Implementación de mapa

--- App.js
+++ App.js
@@ -1,19 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import Icon from 'react-native-vector-icons/MaterialIcons'; // Importamos iconos de Material Icons
 
-export default function App() {
+const App = () => {
+  const [mapRegion, setMapRegion] = useState({
+    
+    latitude: 21.049797041668725, 
+    longitude: -86.84694526164611,
+    latitudeDelta: 0.03,
+    longitudeDelta: 0.03,
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map} region={mapRegion}>
+        <Marker coordinate={mapRegion} title="Ut">
+          {/* Ícono de ubicación personalizado */}
+          <View style={styles.markerIcon}>
+            <Icon name="place" size={40} color="red" />
+          </View>
+        </Marker>
+      </MapView>
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+  markerIcon: {
     alignItems: 'center',
     justifyContent: 'center',
   },
